544bf44263764a88d374501063b48c3ac24edb4b
Kim Honrada <kimhonrada21@gmail.com>
2021-10-27 18:28:06 -0400

    Add helper function files - currently empty

--- server/app.js
+++ server/app.js
@@ -4,6 +4,10 @@ const app = express();
 
 const port = 3000;
 
+const path = require('path');
+
+app.use(express.static(path.join(__dirname, 'dist')));
+
 app.listen(port, () => {
   console.log(`Currently listening to ${port}`);
 });
