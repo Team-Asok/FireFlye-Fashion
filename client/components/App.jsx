a04afa4d4f8c6e5dde97ee8d4cc8b0c2dfbfc80b
James Plier <plier2j@gmail.com>
2021-10-28 14:19:27 -0400

    Fixed Conflict capital A in axios

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Axios from 'axios';
+import axios from 'axios';
 import QandA from './QandA/QandA';
 import RandR from './RandR/RandR';
 import Overview from './Overview/Overview';
@@ -10,4 +10,17 @@ class App extends React.Component {
     this.state = {};
   }
 
+  render() {
+    const { reviews } = this.state;
+    const { products } = this.state;
+    const { qAndA } = this.state;
+    return (
+      <div>
+        <Overview products={{ products }} />
+        <QandA qAndA={{ qAndA }} />
+        <RandR reviews={{ reviews }} />
+      </div>
+    );
+  }
+}
 export default App;
