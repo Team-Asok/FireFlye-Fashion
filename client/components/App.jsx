3fe475ee9b5048fe25750e5b4535517910aa0f13
James Plier <plier2j@gmail.com>
2021-10-28 12:48:21 -0400

    Add state values to app component. Add axios routing methods to app component

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Axios from 'axios';
+import axios from 'axios';
 import QandA from './QandA/QandA.jsx';
 import RandR from './RandR/RandR.jsx';
 import Overview from './Overview/Overview.jsx';
@@ -7,8 +7,59 @@ import Overview from './Overview/Overview.jsx';
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      products: [],
+      reviews: [],
+      qAandA: [],
+      displayedProduct: null,
+    };
   }
+
+  componentDidMount() {
+    this.getAllProducts();
+  }
+
+  /* Sends Axios get request to server to retrieve product data from Product API
+-------------------------------------------------------------------------------- */
+
+  getAllProducts() {
+    const { products } = this.state;
+    const { displayedProduct } = this.state;
+    axios.get('/products')
+      .then((results) => {
+        this.setState({ products: results.data });
+      })
+      .catch((err) => { console.log(`Error: ${err}`); })
+      /* Creates random index and select product based on that index. Sets product to
+      displayed Product */
+      .then(() => {
+        const index = (Math.random() * { products }.length);
+        this.setState({ displayedProduct: { products }[index] });
+      })
+      // Invokes getProductReviews passing in productId for displayed product
+      .then(() => {
+        this.getProductReviews({ displayedProduct }.id);
+      })
+      // Invokes getProductQandA passing in productId for displayed product
+      .then(() => {
+        this.getProductQandA({ displayedProduct }.id);
+      });
+  }
+  /* Sends axios request to server at /reviews with parameter equal to the product id
+  of current display product. Sets state of reviews equal to response.data
+  -------------------------------------------------------------------------------- */
+  getProductReviews(productID) {
+    axios.get(`/reviews/${productID}`)
+
+  }
+
+  getProductQandA() {
+
+  }
+
+
+
+
 
   render() {
     return (
