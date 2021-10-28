509d29e17edb319d3cbc63ebbf37d5166e0d8f1d
James Plier <plier2j@gmail.com>
2021-10-28 14:17:06 -0400

    Fixed conflict linter error

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,29 +1,13 @@
 import React from 'react';
-// eslint-disable-next-line no-unused-vars
 import Axios from 'axios';
-// eslint-disable-next-line import/extensions
-import QandA from './QandA/QandA.jsx';
-// eslint-disable-next-line import/extensions
-import RandR from './RandR/RandR.jsx';
-// eslint-disable-next-line import/extensions
-import Overview from './Overview/Overview.jsx';
+import QandA from './QandA/QandA';
+import RandR from './RandR/RandR';
+import Overview from './Overview/Overview';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {};
   }
-
-  render() {
-    return (
-      <div id="index">
-        Hello world
-        <Overview />
-        <QandA />
-        <RandR />
-      </div>
-    );
-  }
-}
 
 export default App;
