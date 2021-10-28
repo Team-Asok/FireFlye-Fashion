40f1cdd308f1110437631971c3eaf2d9ade10135
Kim Honrada <kimhonrada21@gmail.com>
2021-10-28 19:23:44 -0400

    Add getCart and addCart helper functions

--- server/helpers/cart.js
+++ server/helpers/cart.js
@@ -1,4 +1,43 @@
+const axios = require('axios');
+const config = require('../config');
+
 // GET CART
+const getCart = () => {
+  const options = {
+    url: 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfe/cart',
+    headers: {
+      'User-Agent': 'request',
+      Authorization: config.API_KEY,
+    },
+  };
+  return (
+    axios(options)
+      .then((response) => response.data)
+      .catch((err) => err)
+  );
+};
 
 // POST CART
-  // sku_id
+const addCart = (skuID) => {
+  const options = {
+    url: 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfe/cart',
+    method: 'post',
+    headers: {
+      'User-Agent': 'request',
+      Authorization: config.API_KEY,
+    },
+    data: {
+      sku_id: skuID,
+    },
+  };
+  return (
+    axios(options)
+      .then((response) => response.data)
+      .catch((err) => err)
+  );
+};
+
+module.exports = {
+  addCart,
+  getCart,
+};
