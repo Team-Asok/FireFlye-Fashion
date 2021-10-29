e835e703498ebcf8017895797f313fb8f1fc81c1
Thomas Rose <thomascrose90@gmail.com>
2021-10-29 10:30:05 -0400

    Create and add Cart component to ProductInfo container. Update stylings of Banner, SiteMessage, Gallery, ProductInfo, and ProductDescription to display grid.

--- client/components/Overview/Overview components/ProductInfo.jsx
+++ client/components/Overview/Overview components/ProductInfo.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import RatingLink from './ProductInfo components/RatingLink.jsx';
 import Info from './ProductInfo components/Info.jsx';
 import Styles from './ProductInfo components/Styles.jsx';
+import Cart from './ProductInfo components/Cart.jsx';
 
 const ProductInfo = (props) => (
   <div id="ProductInfo">
     <RatingLink />
     <Info />
     <Styles />
+    <Cart />
   </div>
 );
 
