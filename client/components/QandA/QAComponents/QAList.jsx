3cf2572313575285e23108417ae6f5e867bfb47e
Kim Honrada <kimhonrada21@gmail.com>
2021-10-29 16:12:36 -0400

    Add sample text in QAList to confirm connection, Add test CSS for distinction

---
+++ client/components/QandA/QAComponents/QAList.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const QAList = ({ questions }) => {
+  return (
+    <ul id="QAList">
+    <li>THIS IS A LIST</li>
+    <li>THIS IS A LIST</li>
+    <li>THIS IS A LIST</li>
+    <li>THIS IS A LIST</li>
+    <li>THIS IS A LIST</li>
+  </ul>
+  )
+};
+
+export default QAList;
--- client/components/QandA/QandA.jsx
+++ client/components/QandA/QandA.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
+import QAList from './QAComponents/QAList.jsx';
 
-const QandA = function () {
-  return (
-    <div id="QandA">
-      Questions and Answers
-    </div>
-  );
-};
+class QandA extends React.Component {
+  constructor({ qAndA }) {
+    super({ qAndA });
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div id="QandA">
+        Questions and Answers
+        <QAList />
+      </div>
+    );
+  }
+}
 
 export default QandA;
