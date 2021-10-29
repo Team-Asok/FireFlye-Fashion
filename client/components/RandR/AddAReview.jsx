b2b7b3c5805bddcb060a3c2005ea8b5dbb8cea63
James Plier <plier2j@gmail.com>
2021-10-29 15:43:00 -0400

    Add more reviews and add a review button + styles

---
+++ client/components/RandR/AddAReview.jsx
@@ -0,0 +1,7 @@
+import React from 'React'
+
+const AddAReview = (props) => (
+  <button id="add-a-review" type='button' text="Add a Review +">Add a Review</button>
+);
+
+export default AddAReview;
--- client/components/RandR/ReviewList.jsx
+++ client/components/RandR/ReviewList.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import styled from 'styled-components';
 import ReviewTile from './ReviewTile.jsx';
+import AddAReview from './AddAReview.jsx';
+import MoreReviews from './MoreReviews.jsx';
 
 const StyledDiv = styled.div`
    border-style: dotted;
@@ -11,11 +13,15 @@ const StyledDiv = styled.div`
 const ReviewList = (props) => {
   if (props.reviews.results) {
     return (
-    <StyledDiv id="reviewlist">
-      <ul id="reviewListTilescontainer">
-      {props.reviews.results.map((review) => <ReviewTile review={review} />)}
-      </ul>
-    </StyledDiv>
+      <>
+        <StyledDiv id="reviewlist">
+          <ul id="reviewListTilescontainer">
+          {props.reviews.results.map((review) => <ReviewTile review={review} />)}
+          </ul>
+          <AddAReview />
+          <MoreReviews />
+        </StyledDiv>
+      </>
     );
   }
   return null;
