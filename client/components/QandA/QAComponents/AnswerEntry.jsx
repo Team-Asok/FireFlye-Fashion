d814a10a8549f681610da78ca004afd7026877f4
Kim Honrada <kimhonrada21@gmail.com>
2021-10-30 11:18:23 -0400

    Add Load more answer button(no functionality yet) Update component to render properly even without answers

--- client/components/QandA/QAComponents/AnswerEntry.jsx
+++ client/components/QandA/QAComponents/AnswerEntry.jsx
@@ -3,20 +3,24 @@ import React from 'react';
 const AnswerEntry = ({ answer }) => {
   // line 5, prints single answer
  // console.log('this is answers ', answer[0][1])
-
-  return (
-    <div id="AnswerEntry">
-      <div>
-      Answer: {answer[0][1].body}
+  if (!answer[0]) {
+    return null;
+  } else {
+    return (
+      <div id="AnswerEntry">
+        <div>
+        Answer: {answer[0][1].body}
+        </div>
+        <div>
+        by: {answer[0][1].answerer_name}
+        </div>
+        <div>
+        when: {answer[0][1].date}
+        </div>
+        <button>Load More Answers</button>
       </div>
-      <div>
-      by: {answer[0][1].answerer_name}
-      </div>
-      <div>
-      when: {answer[0][1].date}
-      </div>
-    </div>
-  )
+    )
+  }
 };
 
 export default AnswerEntry;
