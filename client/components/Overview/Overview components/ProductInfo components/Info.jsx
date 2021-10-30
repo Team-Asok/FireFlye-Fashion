439bbc1514c2d8e93264fa2654c6303f15aa0241
Thomas Rose <thomascrose90@gmail.com>
2021-10-30 10:27:23 -0400

    Add conditional render to Info component. Dynamically render category, name, and price. Add styled div for testing area.

--- client/components/Overview/Overview components/ProductInfo components/Info.jsx
+++ client/components/Overview/Overview components/ProductInfo components/Info.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
+import styled from 'styled-components';
 
-const Info = (props) => (
-  <div id="info">
-    Category
-    Product Title
-    Product Price
-  </div>
-);
+const StyledPInfoContainer = styled.div`
+  background-color: lightcoral;
+`;
+
+const Info = (props) => {
+  if (props.product) {
+    return (
+      <StyledPInfoContainer id="info">
+        {props.product.category}
+        {props.product.name}
+        {props.product.default_price}
+      </StyledPInfoContainer>
+    );
+  }
+  return null;
+};
 
 export default Info;
