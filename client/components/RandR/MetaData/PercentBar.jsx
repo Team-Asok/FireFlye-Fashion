9c64d952271915b183926a92606cf76a12010898
James Plier <plier2j@gmail.com>
2021-10-30 18:28:29 -0400

    Fix NaN error in star rating table

--- client/components/RandR/MetaData/PercentBar.jsx
+++ client/components/RandR/MetaData/PercentBar.jsx
@@ -1,17 +1,23 @@
 import React from 'react';
 
 const PercentBar = (props) => {
-  console.log(props.percentage)
+
+  if (isNaN(props.percentage)) {
+     var percentage = 0;
+  } else {
+     var percentage = props.percentage;
+  }
+
   return (
     <td style={{'width': '75%'}}>
       <svg width="250" height="15">
         <defs>
-            <linearGradient id={`grad${props.percentage}`} x1="0%" y1="0%" x2="100%" y2="0%">
-              <stop offset={`${props.percentage}%`} style={{'stop-color':'rgb(255,255,0)', 'stop-opacity':1}} />
-              <stop offset={`${props.percentage + 1}%`} style={{'stop-color':'rgb(255,0,0)', 'stop-opacity':1}} />
+            <linearGradient id={`grad${percentage}`} x1="0%" y1="0%" x2="100%" y2="0%">
+              <stop offset={`${percentage}%`} style={{'stop-color':'rgb(255,255,0)', 'stop-opacity':1}} />
+              <stop offset={`${percentage + 1}%`} style={{'stop-color':'rgb(255,0,0)', 'stop-opacity':1}} />
             </linearGradient>
         </defs>
-        <rect width="250" height="10" fill={`url(#grad${props.percentage})`}/>
+        <rect width="250" height="10" fill={`url(#grad${percentage})`}/>
       </svg>
     </td>
   );
