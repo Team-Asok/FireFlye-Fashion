5694a0f76a18633a1afa7d8cee041faee57bfe3d
Thomas Rose <thomascrose90@gmail.com>
2021-11-01 10:43:51 -0400

    Organize ProductDescription component into a flexbox format. Create separator div and style it. Introduce list item section to be dynamically rendered later.

--- client/components/Overview/Overview components/ProductDescription.jsx
+++ client/components/Overview/Overview components/ProductDescription.jsx
@@ -1,16 +1,31 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const StyledProdDesc = styled.div`
+  background-color: yellow;
+  display: flex;
+  flex-direction: row;
+`;
 
 const ProductDescription = ({currentProd}) => {
   if (currentProd) {
     return (
-      <div id="ProductDescription">
-        <h3>
-          {currentProd.slogan}
-        </h3>
-        <span>
-          {currentProd.description}
-        </span>
-      </div>
+      <StyledProdDesc id="ProductDescription">
+        <div>
+          <h3>
+            {currentProd.slogan}
+          </h3>
+          <span>
+            {currentProd.description}
+          </span>
+        </div>
+        <div id="Separator" />
+        <ul>
+          <li>
+            HardCoding!
+          </li>
+        </ul>
+      </StyledProdDesc>
     );
   }
   return null;
