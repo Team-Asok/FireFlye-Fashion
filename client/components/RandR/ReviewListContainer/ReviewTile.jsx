5fca1c2b3ebec31d2bdf6c080f71f7c8ed3cda54
James Plier <plier2j@gmail.com>
2021-11-01 14:31:31 -0400

    Add sort by newest functionality

--- client/components/RandR/ReviewListContainer/ReviewTile.jsx
+++ client/components/RandR/ReviewListContainer/ReviewTile.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import moment from 'moment'
 
 const ReviewTile = function(props) {
   if (props.review) {
     return (
      <div list-style-type="none" id="reviewtile">
        <div>star tile placeholder</div>
-       <div id="timestamp">{props.review.date}</div>
+       <div id="timestamp">{moment(props.review.date).format('MMM do YYYY')}</div>
        <div id="review-tile-summary">{props.review.summary}</div>
        <div className="review-body" id="review-tile-body">{props.review.body}</div>
        <div className="review-body" id="review-tile-response">{props.review.response}</div>
--- client/components/RandR/ReviewListContainer/sortReviewsFunction.js
+++ client/components/RandR/ReviewListContainer/sortReviewsFunction.js
@@ -1,6 +1,8 @@
-const sortReviews = (reviews, criteria) => {
+import moment from 'moment';
 
-  console.log(reviews);
+const sortReviews = (reviews, criteria) => {
+  var reviewsCopy = reviews.slice();
+
   var sortByHelpful = (reviews) => {
     var compareFunction = (a, b) => {
       if (a.helpfulness < b.helpfulness) {
@@ -17,14 +19,66 @@ const sortReviews = (reviews, criteria) => {
 
   var sortByDate = (reviews) => {
 
+    var compareFunction = (a, b) => {
+
+      // Date parsing functions ----------------------->
+      var getYear = (string) => {
+         var year = string.substring(0, 4)
+         return parseInt(year, 10);
+      }
+      var getMonth = (string) => {
+        var month = string.substring(5, 7)
+        return parseInt(month, 10);
+      }
+      var getDay = (string) => {
+        var day = string.substring(9, 11);
+        return parseInt(day, 10);
+      }
+
+      // Comparison Conditionals
+      if (getYear(a.date) > getYear(b.date)) {
+        return -1
+      }
+      if (getMonth(a.date) > getMonth(b.date)) {
+          return -1
+      }
+      if (getDay(a.date) > getDay(b.date)) {
+        return -1
+      }
+      if (getYear(a.date) < getYear(b.date)) {
+        return 1
+      }
+      if (getMonth(a.date) < getMonth(b.date)) {
+          return 1
+      }
+      if (getDay(a.date) < getDay(b.date)) {
+        return 1
+      }
+
+      return 0;
+    }
+
+  var sortedReviews = reviews.sort(compareFunction)
+
+  return sortedReviews;
+  }
+
+
+
+  var sortByRelevance = (reviews) => {
+    sortedByHelpfulness = reviews(sortByHelpful);
   }
 
   if (criteria === 'helpful') {
-    var sortedReviews = sortByHelpful(reviews);
+    var sortedReviews = sortByHelpful(reviewsCopy);
     return sortedReviews;
   }
   if (criteria === 'relevance') {
-    return reviews.sort();
+    return reviewsCopy;
+  }
+  if (criteria === 'newest') {
+    var sortedReviews = sortByDate(reviewsCopy);
+    return sortedReviews;
   }
 
 }
