adefd49ef4b16fefd8acede7e00f3879592a6404
Kim Honrada <kimhonrada21@gmail.com>
2021-11-01 10:39:35 -0400

    Handle the rendering of load more answer buttons depending on the number of questions

--- client/components/QandA/QAComponents/AnswerEntry.jsx
+++ client/components/QandA/QAComponents/AnswerEntry.jsx
@@ -5,8 +5,21 @@ const AnswerEntry = ({ answer }) => {
 
   if (!answer[0]) {
     return null;
+  } else if (answer.length === 1) {
+    return (
+      <div id="AnswerEntry">
+        <div>
+        Answer: {answer[0][1].body}
+        </div>
+        <div>
+        by: {answer[0][1].answerer_name}
+        </div>
+        <div>
+        when: {answer[0][1].date}
+        </div>
+      </div>
+    )
   } else {
-    //let answer = answer[0][1];
     return (
       <div id="AnswerEntry">
         <div>
