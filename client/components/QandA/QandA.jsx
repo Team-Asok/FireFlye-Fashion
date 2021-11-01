40ca9b09de83608eb7b1e5e075d37e5dd032547d
Kim Honrada <kimhonrada21@gmail.com>
2021-11-01 11:14:38 -0400

    Add more answered question to Q and A component, add a temp styling

--- client/components/QandA/QandA.jsx
+++ client/components/QandA/QandA.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import QAList from './QAComponents/QAList.jsx';
 import AddQuestion from './QAComponents/AddQuestion.jsx'
+import MoreAnsweredQuestions from './QAComponents/MoreAnsweredQuestions.jsx'
 
 const QandA = ({ qAndA }) => {
   return (
@@ -8,6 +9,7 @@ const QandA = ({ qAndA }) => {
       Questions and Answers
       <QAList questions={qAndA.results}/>
       <AddQuestion />
+      <MoreAnsweredQuestions />
     </div>
   );
 };
