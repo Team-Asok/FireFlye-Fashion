5bdb89cc71dccb21d6febfe1abb0b1cc83478799
Kim Honrada <kimhonrada21@gmail.com>
2021-11-01 09:26:26 -0400

    Add variable for accepting answer in AnswerEntry.jsx Change grid row for QA List

--- client/components/QandA/QAComponents/AnswerEntry.jsx
+++ client/components/QandA/QAComponents/AnswerEntry.jsx
@@ -1,21 +1,21 @@
 import React from 'react';
 
 const AnswerEntry = ({ answer }) => {
-  // line 5, prints single answer
- // console.log('this is answers ', answer[0][1])
+
   if (!answer[0]) {
     return null;
   } else {
+    let answer = answer[0][1];
     return (
       <div id="AnswerEntry">
         <div>
-        Answer: {answer[0][1].body}
+        Answer: {answer.body}
         </div>
         <div>
-        by: {answer[0][1].answerer_name}
+        by: {answer.answerer_name}
         </div>
         <div>
-        when: {answer[0][1].date}
+        when: {answer.date}
         </div>
         <button>Load More Answers</button>
       </div>
