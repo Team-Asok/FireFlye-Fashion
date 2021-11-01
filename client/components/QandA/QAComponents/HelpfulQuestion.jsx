e477ac8b3329dc9e2b9cfb5860d3e27de9587700
Kim Honrada <kimhonrada21@gmail.com>
2021-11-01 14:32:26 -0400

    Add report answer functionality

--- client/components/QandA/QAComponents/HelpfulQuestion.jsx
+++ client/components/QandA/QAComponents/HelpfulQuestion.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import axios from 'axios';
 
 const HelpfulQuestion = ({questionID}) => {
+
   const updateQuestionHelpful = (targetQuestion) => {
     axios.put(`/qa/questions/${targetQuestion}/helpful`, {
       params : targetQuestion
