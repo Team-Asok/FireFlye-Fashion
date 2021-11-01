ccd50b3773d713f4efd12b9aa2fa397027887f33
Kim Honrada <kimhonrada21@gmail.com>
2021-11-01 09:49:43 -0400

    Add dev mode to webpack config Revert changes to answerEntry.jsx

---
+++ client/components/QandA/QAComponents/AddQuestion.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+class AddQuestion extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      entry: ''
+    }
+  }
+  handleChange () {
+
+  }
+  handleSubmit () {
+
+  }
+  render() {
+    return (
+      <form id="AddQuestion">
+      </form>
+    )
+  }
+}
+// checking
+export default AddQuestion;
