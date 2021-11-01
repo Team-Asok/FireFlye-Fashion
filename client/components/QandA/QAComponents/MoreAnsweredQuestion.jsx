0302c87feeb2f7c5c2fd11ba24b5e3a240f910e8
Kim Honrada <kimhonrada21@gmail.com>
2021-11-01 11:04:43 -0400

    Create more answered question component

---
+++ client/components/QandA/QAComponents/MoreAnsweredQuestion.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const MoreAnsweredQuesions = (props) => {
+  return (
+    <button id="more-answered-questions" type="button">MORE ANSWERED QUESTIONS</button>
+  )
+}
+
+export default MoreAnsweredQuesions;
