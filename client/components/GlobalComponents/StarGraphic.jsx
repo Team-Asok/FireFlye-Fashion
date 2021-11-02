628b55b83af2304067c3a10cddabcaf56c67db6c
James Plier <plier2j@gmail.com>
2021-11-02 15:44:01 -0400

    Star graphic in progress

--- client/components/GlobalComponents/StarGraphic.jsx
+++ client/components/GlobalComponents/StarGraphic.jsx
@@ -3,13 +3,11 @@ import React from 'react'
 const StarGraphic = (props) => {
   return (
     <div>
-
-
-      <svg xmlns="http://www.w3.org/2000/svg" className="star" viewBox="0 0 20 20" fill="url(#test)">
+     <svg xmlns="http://www.w3.org/2000/svg" className="star" viewBox="0 0 20 20" fill="url(#test)">
       <defs>
             <linearGradient id={`test`} x1="0%" y1="0%" x2="100%" y2="0%">
-              <stop offset={`20%`} style={{'stopColor':'rgb(255,255,0)', 'stopOpacity':1}} />
-              <stop offset={`80%`} style={{'stopColor':'rgb(255,0,0)', 'stopOpacity':1}} />
+              <stop offset={`50%`} style={{'stopColor':'rgb(0, 0, 0)', 'stopOpacity':1}} />
+              <stop offset={`51%`} style={{'stopColor':'rgb(255, 255, 255)', 'stopOpacity':1}} />
             </linearGradient>
         </defs>
         <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
@@ -27,6 +25,7 @@ const StarGraphic = (props) => {
         <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
       </svg>
   </div>
+
   )
 }
 
