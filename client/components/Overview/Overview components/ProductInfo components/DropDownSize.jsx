d687bd2759fd553757c7ce1050efc6a9c5d22172
Thomas Rose <thomascrose90@gmail.com>
2021-11-04 12:48:21 -0400

    Add conditional to render a disabled select box with an out of stock message if there is no inventory for selected style.

--- client/components/Overview/Overview components/ProductInfo components/DropDownSize.jsx
+++ client/components/Overview/Overview components/ProductInfo components/DropDownSize.jsx
@@ -1,12 +1,22 @@
 import React from 'react';
 
-const DropDownSize = (props) => (
-  <select id={`${props.default}}`} onChange={props.select}>
-    <option selected disabled>Select Size</option>
-    {props.sizes.map((size) => {
-      return <option value={size}>{size}</option>
-    })}
-  </select>
-);
+const DropDownSize = (props) => {
+  if (props.sizes.length === 0) {
+    return (
+      <select disabled>
+        <option>OUT OF STOCK</option>
+      </select>
+    );
+  }
+
+  return (
+    <select id={`${props.default}}`} onChange={props.select}>
+      <option selected disabled>Select Size</option>
+      {props.sizes.map((size) => {
+        return <option value={size}>{size}</option>
+      })}
+    </select>
+  );
+};
 
 export default DropDownSize;
