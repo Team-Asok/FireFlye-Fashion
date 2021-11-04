dd18314c3b479a32a2f70a3236724fbecf5a8183
Kim Honrada <kimhonrada21@gmail.com>
2021-11-03 23:56:50 -0400

    Sort questions in increasing order of number of answers

--- client/components/QandA/QAComponents/QAList.jsx
+++ client/components/QandA/QAComponents/QAList.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import QAListEntry from './QAListEntry.jsx';
+import sortByAnswers from './sortByAnswers.js'
 
 const QAList = ({ questions, getProductQandA, productID }) => {
-  if (!questions) {
+  let quesArr = sortByAnswers(questions)
+
+  if (!quesArr) {
     return (
     <ul id="qa-list">BE THE FIRST ONE TO ASK A QUESTION</ul>
     )
   } else {
     return (
       <ul id="qa-list">
-        {questions.map((question) =>
+        {quesArr.map((question) =>
         <QAListEntry productID={productID} getProductQandA={getProductQandA} key={question.question_id} question={question} />
         )}
       </ul>
