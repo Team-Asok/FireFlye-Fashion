0486f76b220c276f5664f703b27e41b30964ecfe
Kim Honrada <kimhonrada21@gmail.com>
2021-11-05 16:45:15 -0400

    Change input boxes in modals into textarea

--- client/components/QandA/QAComponents/AnswerModal.jsx
+++ client/components/QandA/QAComponents/AnswerModal.jsx
@@ -54,7 +54,7 @@ class AnswerModal extends React.Component {
             <form onSubmit={this.handleSubmit}>
               <div className="modal-question">
               <label> ANSWER:*
-                <input type="text" name="question" maxLength="1000" onChange={this.handleChange} required/>
+                <textarea type="text" name="question" maxLength="1000" onChange={this.handleChange} required/>
               </label>
               </div>
               <div className="modal-name">
