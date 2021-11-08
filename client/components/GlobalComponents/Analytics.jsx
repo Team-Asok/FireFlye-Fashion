b3be110daf1c2adc7b3230242d0d528149a55416
James Plier <plier2j@gmail.com>
2021-11-08 11:20:01 -0500

    Add analytic trackerFunction

---
+++ client/components/GlobalComponents/Analytics.jsx
@@ -0,0 +1,10 @@
+import react from 'react'
+
+const trackAnalytic = (widget) => {
+  console.log(event.target) // needs to be stringified
+  console.log(new Date();) // needs to be stringified
+
+}
+
+
+export default trackAnalytic;
