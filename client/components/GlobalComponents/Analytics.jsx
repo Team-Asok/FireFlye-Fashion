91f2074441ea861fb0b76b18bafc319563594258
James Plier <plier2j@gmail.com>
2021-11-08 11:32:22 -0500

    accidnetal staged file

---
+++ client/components/GlobalComponents/Analytics.jsx
@@ -0,0 +1,11 @@
+import react from 'react'
+
+const trackAnalytic = (widget) => {
+  console.log(event.target) // needs to be stringified
+  console.log(new Date();) // needs to be stringified
+  // widget
+
+}
+
+
+export default trackAnalytic;
