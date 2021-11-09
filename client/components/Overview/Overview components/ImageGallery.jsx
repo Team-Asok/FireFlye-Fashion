3bd1831817f9e84c6a61a817938fb473d69f2123
Thomas Rose <thomascrose90@gmail.com>
2021-11-09 12:29:36 -0500

    Update CSS to contain main image in gallery.

--- client/components/Overview/Overview components/ImageGallery.jsx
+++ client/components/Overview/Overview components/ImageGallery.jsx
@@ -3,26 +3,22 @@ import ImageThumbnail from './ImageThumbnail.jsx';
 import NoImage from './NoImage.png';
 
 const styling = {
-  container: {
-    display: 'flex',
-    flexDirection: 'row',
-  },
   main: {
     flex: 1,
-    maxWidth: 1000,
-    height: "auto",
+    maxWidth: '100%',
     maxHeight: 800,
+    resizeMode: 'contain',
+    overflow: 'hidden',
   },
   tn: {
     display: 'flex',
     flexDirection: 'column',
     justifyContent: 'start',
-
   },
 };
 
 const Gallery = ({ style, photo, updatePhoto }) => (
-  <div id="Gallery" style={styling.container}>
+  <div id="Gallery">
     { style.photos[0].thumbnail_url
       ? <div style={styling.tn}>
           {style.photos.slice(0, 7).map((photo, index) => <ImageThumbnail key={index} photo={photo} updatePhoto={updatePhoto} />)}
