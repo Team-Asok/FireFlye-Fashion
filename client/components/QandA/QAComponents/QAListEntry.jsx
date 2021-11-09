6b7b819ca81a92c8d66eb48954d21e8f0d48993e
Kim Honrada <kimhonrada21@gmail.com>
2021-11-08 19:46:18 -0500

    Refactor QAList and QAListEntry into class components

--- client/components/QandA/QAComponents/QAListEntry.jsx
+++ client/components/QandA/QAComponents/QAListEntry.jsx
@@ -3,17 +3,39 @@ import QuestionEntry from './QuestionEntry.jsx';
 import AnswerEntry from './AnswerEntry.jsx';
 import sortQA from './sortQA.js'
 
-const QAListEntry = ({ question, getProductQandA, productID, productName }) => {
-  let arrAnswer = Object.values(question.answers)
+class QAListEntry extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      answers: Object.values(this.props.question.answers),
+      question: this.props.question
+    }
+  }
 
-  return (
-    <li>
-      <div id="question-container">
-        <QuestionEntry helpfulness={question.question_helpfulness} productID={productID} questionBody={question.question_body} questionID={question.question_id} getProductQandA={getProductQandA} productName={productName} />
-        <AnswerEntry answers={arrAnswer} questionBody={question.question_body} productName={productName} />
-      </div>
-    </li>
-  )
-};
+  render() {
+    return (
+      <li>
+        <div id="question-container">
+          <div id="question-entry">
+            <QuestionEntry
+              helpfulness={this.state.question.question_helpfulness}
+              productID={this.props.productID}
+              productName={this.props.productName}
+              questionBody={this.state.question.question_body}
+              getProductQandA={this.getProductQandA}
+            />
+          </div>
+          <div id="answer-entry">
+            <AnswerEntry
+            answers={this.state.answers}
+            questionBody={this.state.question.question_body}
+            productName={this.props.productName}
+            />
+          </div>
+        </div>
+      </li>
+    )
+  }
+}
 
 export default QAListEntry;
