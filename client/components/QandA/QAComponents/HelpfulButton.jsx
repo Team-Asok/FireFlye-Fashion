46152c2dfeaa76318f574a84cd924fb514140074
Kim Honrada <kimhonrada21@gmail.com>
2021-11-08 23:52:44 -0500

    Helpful button jsx added handling for response

--- client/components/QandA/QAComponents/HelpfulButton.jsx
+++ client/components/QandA/QAComponents/HelpfulButton.jsx
@@ -6,15 +6,12 @@ const HelpfulButton = ({ id, path, helpfulness }) => {
     axios.put(path, {
       params: targetId
     })
-      .then(response => {
-      })
+      .then(response => response)
       .catch(err => console.log(err));
   }
 
   return (
-    <button onClick={() => {
-      updateHelpful(id);
-    }} id="helpful-button" type="button" >Helpful? Yes({helpfulness})</button>
+    <button onClick={() => {updateHelpful(id);}} id="helpful-button" type="button" >Helpful? Yes({helpfulness})</button>
   )
 }
 
