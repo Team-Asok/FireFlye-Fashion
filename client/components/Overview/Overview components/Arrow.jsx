471c77fb0a1f8ead78a40bb4beb0e98828d25404
Thomas Rose <thomascrose90@gmail.com>
2021-11-11 14:25:49 -0500

    Add styling to maintain space in thumbnail arrows regardless of visibility.

--- client/components/Overview/Overview components/Arrow.jsx
+++ client/components/Overview/Overview components/Arrow.jsx
@@ -1,13 +1,7 @@
 import React from 'react';
 
-const styling = {
-  position: {
-    marginTop: '35%',
-  },
-};
-
 const Arrow = ({ changePhoto, glyph }) => (
-  <div style={styling.position} onClick={() => changePhoto()}>
+  <div onClick={() => changePhoto()}>
     {glyph}
   </div>
 );
--- client/components/Overview/Overview components/ImageGallery.jsx
+++ client/components/Overview/Overview components/ImageGallery.jsx
@@ -23,8 +23,13 @@ const styling = {
     flexDirection: 'row',
     justifyContent: 'center',
   },
-  hidden: {
+  hiddenMain: {
     height: 50,
+    width: 50,
+    visibility: 'hidden',
+  },
+  hiddenTN: {
+    height: 72,
     width: 50,
     visibility: 'hidden',
   },
@@ -34,9 +39,9 @@ const Gallery = ({ style, photo, photos, updatePhoto, currentIndex, previousSlid
   <div id="Gallery">
     { style.photos[0].thumbnail_url
       ? <div style={styling.tn}>
-          {currentIndex !== 0 ? <Arrow glyph="&#8593;" changePhoto={previousSlide} /> : null}
+          {currentIndex !== 0 ? <Arrow glyph="&#8593;" changePhoto={previousSlide} /> : <div style={styling.hiddenTN} />}
           {style.photos.slice(0, 7).map((photo, index) => <ImageThumbnail key={index} photo={photo} updatePhoto={updatePhoto} />)}
-          {currentIndex !== photos.length - 1 ? <Arrow glyph="&#8595;" changePhoto={nextSlide} /> : null}
+          {currentIndex !== photos.length - 1 ? <Arrow glyph="&#8595;" changePhoto={nextSlide} /> : <div style={styling.hiddenTN} />}
         </div>
       : null
     }
@@ -44,11 +49,11 @@ const Gallery = ({ style, photo, photos, updatePhoto, currentIndex, previousSlid
     {photo.url
       ? (
         <div style={styling.arrows}>
-          {currentIndex !== 0 ? <Arrow glyph="&#8592;" changePhoto={previousSlide} /> : <div style={styling.hidden} />}
+          {currentIndex !== 0 ? <Arrow glyph="&#8592;" changePhoto={previousSlide} /> : <div style={styling.hiddenMain} />}
           <div style={styling.main}>
             <img className="main-image" src={photo.url} alt="main image gallery image"/>
           </div>
-          {currentIndex !== photos.length - 1 ? <Arrow glyph="&#8594;" changePhoto={nextSlide} /> : <div style={styling.hidden} />}
+          {currentIndex !== photos.length - 1 ? <Arrow glyph="&#8594;" changePhoto={nextSlide} /> : <div style={styling.hiddenMain} />}
         </div>
       )
       : <img src={NoImage} style={styling.noImage} alt="placeholder image"/>}
