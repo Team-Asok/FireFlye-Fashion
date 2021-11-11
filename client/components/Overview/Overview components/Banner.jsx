0bf7fa48c0f251927783a564800763413e319081
James Plier <plier2j@gmail.com>
2021-11-11 12:08:30 -0500

    Change logo color

--- client/components/Overview/Overview components/Banner.jsx
+++ client/components/Overview/Overview components/Banner.jsx
@@ -4,7 +4,7 @@ import FyreFlye from './FyreFlye.png'
 const Banner = (props) => (
   <div id="Banner">
     FyreFlye Fashn
-    <img src={FyreFlye} height="150px" width="auto" alt="fireflye logo"/>
+    <img style={{'margin': '10px'}} src={FyreFlye} height="150px" width="auto" alt="fireflye logo"/>
   </div>
 
 );
