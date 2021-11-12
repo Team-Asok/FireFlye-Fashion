f3af989db3604b3567f21748d8bf070964b07457
Thomas Rose <thomascrose90@gmail.com>
2021-11-12 01:04:01 -0500

    Refactor gallery. Main gallery into separate component to generate different views.

--- client/components/Overview/Overview components/ImageGallery.jsx
+++ client/components/Overview/Overview components/ImageGallery.jsx
@@ -8,105 +8,43 @@ import React from "react";
 import ImageThumbnail from "./ImageThumbnail.jsx";
 import NoImage from "./NoImage.png";
 import Arrow from "./Arrow.jsx";
-
-const styling = {
-  left: {
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "start",
-  },
-  tn: {
-    overflow: "scroll",
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "start",
-    maxHeight: 510,
-  },
-  main: {
-    overflow: 'hidden',
-  },
-  noImage: {
-    height: 500,
-    width: 500,
-    marginTop: "10%",
-    marginLeft: "20%",
-  },
-  arrows: {
-    display: 'flex',
-    flexDirection: 'row',
-    justifyContent: 'center',
-  },
-  arrowSpace: {
-    marginTop: 360,
-  },
-  hiddenMain: {
-    height: 50,
-    width: 50,
-    visibility: 'hidden',
-  },
-  hiddenTN: {
-    height: 72,
-    width: 50,
-    visibility: 'hidden',
-  },
-};
-
-// const Gallery = ({ style, currentPhoto, photos, updatePhoto, currentIndex, previousSlide, nextSlide }) => (
-//   <div id="Gallery">
-//     { style.photos[0].thumbnail_url
-//       ? <div style={styling.left} key={style.style_id}>
-//           <div style={styling.tn}>
-//             {style.photos.map((photo, index) => <ImageThumbnail key={index} photo={photo} updatePhoto={updatePhoto} current={currentPhoto}/>)}
-//           </div>
-//         </div>
-//        : null}
-
-//     {currentPhoto.url
-//       ? (
-//         <div style={styling.arrows}>
-//           {currentIndex !== 0 ? <Arrow glyph="&#8678;" changePhoto={previousSlide} style={styling.arrowSpace} /> : <div style={styling.hiddenMain} />}
-//           <div style={styling.main}>
-//             <img className="main-image" src={currentPhoto.url} alt="main image gallery image"/>
-//           </div>
-//           {currentIndex !== photos.length - 1 ? <Arrow glyph="&#8680;" changePhoto={nextSlide} style={styling.arrowSpace} /> : <div style={styling.hiddenMain} />}
-//         </div>
-//       ) : (
-//         <img src={NoImage} style={styling.noImage} alt="placeholder image" />
-//       )}
-//   </div>
-// );
+import ImageMain from './ImageMain.jsx';
 
 class Gallery extends React.Component {
   constructor(props) {
     super(props)
-    this.state = {}
+    this.state = {
+      expanded: false
+    }
+    this.changeView = this.changeView.bind(this);
+    this.renderView = this.renderView.bind(this);
+  }
+
+  changeView() {
+    this.setState({expanded: !this.state.expanded}, () => {
+      this.renderView();
+    });
+  }
+
+  renderView() {
+    if (!this.state.expanded) {
+    } else if (this.state.expanded) {
+
+    }
   }
 
   render() {
     return (
-      <div id="Gallery">
-        { this.props.style.photos[0].thumbnail_url
-          ? <div style={styling.left} key={this.props.style.style_id}>
-              <div style={styling.tn}>
-                {this.props.style.photos.map((photo, index) => <ImageThumbnail key={index} photo={photo} updatePhoto={this.props.updatePhoto} current={this.props.currentPhoto}/>)}
-              </div>
-            </div>
-          : null}
-
-        {this.props.currentPhoto.url
-          ? (
-            <div style={styling.arrows}>
-              {this.props.currentIndex !== 0 ? <Arrow glyph="&#8678;" changePhoto={this.props.previousSlide} style={styling.arrowSpace} /> : <div style={styling.hiddenMain} />}
-              <div style={styling.main}>
-                <img className="main-image" src={this.props.currentPhoto.url} alt="main image gallery image"/>
-              </div>
-              {this.props.currentIndex !== this.props.photos.length - 1 ? <Arrow glyph="&#8680;" changePhoto={this.props.nextSlide} style={styling.arrowSpace} />
-              : <div style={styling.hiddenMain} />}
-            </div>
-          ) : (
-            <img src={NoImage} style={styling.noImage} alt="placeholder image" />
-          )}
-      </div>
+      <ImageMain
+        updatePhoto={this.props.updatePhoto}
+        previousSlide={this.props.previousSlide}
+        nextSlide={this.props.nextSlide}
+        style={this.props.style}
+        currentPhoto={this.props.currentPhoto}
+        photos={this.props.photos}
+        currentIndex={this.props.currentIndex}
+        changeView={this.changeView}
+      />
     )
   }
 }
